5f1ff90436775f146e9b19f87bae11ac72219641
edouard06 <edouarrddd@gmail.com>
2025-02-19 10:23:45 +0100

    Modele et importation

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { CountryData } from '../models/country.model';
 
 @Injectable({
   providedIn: 'root',
