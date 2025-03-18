f5a93253af9b1ad56ab4da7d146ce870cd13e492
edouard06 <edouarrddd@gmail.com>
2025-03-18 13:57:39 +0100

    error loading data message

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,30 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
-import { OlympicCountry } from '../models/olympic';
+import { OlympicCountry } from '../models/olympic'; // ✅ Typage fort
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
+
   private olympics$ = new BehaviorSubject<OlympicCountry[] | null>(null);
-  
+
   constructor(private http: HttpClient) {}
 
-  loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
-      tap((value) => this.olympics$.next(value)),
-      catchError((error, caught) => {
-        console.error(error);
+  
+  loadInitialData(): Observable<OlympicCountry[]> {
+    return this.http.get<OlympicCountry[]>(this.olympicUrl).pipe(
+      tap((data) => this.olympics$.next(data)),
+      catchError((error) => {
+        console.error('[OlympicService] Error loading data :', error);
         this.olympics$.next(null);
-        return caught;
+        return throwError(() => new Error('Error loading olympic data :.'));
       })
     );
   }
 
-  getOlympics() {
+ 
+  getOlympics(): Observable<OlympicCountry[] | null> {
     return this.olympics$.asObservable();
   }
 }
